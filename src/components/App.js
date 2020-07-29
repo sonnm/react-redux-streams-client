8ff52e304d55955a325675e59d7742eb8b7cf747
sonnm <sono.online@gmail.com>
2020-07-29 16:42:23 +0700

    Never gonna say goodbye

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ const App = () => {
   return (
     <div className="ui container">
       <BrowserRouter>
+        {process.env.NODE_ENV}
         {process.env.REACT_APP_API_ENDPOINT}
         <Header />
         <Route path="/" exact component={StreamList} />
