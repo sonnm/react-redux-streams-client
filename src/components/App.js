c2842d956fca0a215ae1a3eeabd82e5adfb243a5
sonnm <sono.online@gmail.com>
2020-07-29 16:28:49 +0700

    more stuff

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,6 @@ const App = () => {
   return (
     <div className="ui container">
       <BrowserRouter>
-        {process.env}
         {process.env.REACT_APP_API_ENDPOINT}
         <Header />
         <Route path="/" exact component={StreamList} />
